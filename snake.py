33dee750ebc5b1373c9bec975ddb34c4010d3783
ruanpienaar <ruan800@gmail.com>
2015-02-17 03:04:53 +0000

    ..

--- snake.py
+++ snake.py
@@ -32,11 +32,10 @@ while True:
 
     if ver_pos == 7:
         go_left = False
-        #position = (position[0], position[1]-1)
         if go_down:
-            position = (position[0]+1, position[1])
+            position = (position[0]+1, position[1]-1)
         else:
-            position = (position[0]-1, position[1])
+            position = (position[0]-1, position[1]-1)
     elif ver_pos == 0:
         go_left = True
         if go_down:
