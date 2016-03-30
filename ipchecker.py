7e3946e36b4b0552b3fbea505ad4b6d679f18f18
ruanpienaar <ruan800@gmail.com>
2016-03-31 00:04:42 +0100

    Added python script to record your internet address, and email, if there is a change. this can usually be run by a cron

---
+++ ipchecker.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+import sys
+import urllib
+import json
+import smtplib
+
+default_details = '{"ip": "0.0.0.0", "latlong": "0,0", "country": "X", "city": "X", "user-agent": "UA"}'
+
+def send_email(user, pwd, recipient, subject, body):
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
+
+def compare_stored_ip( stored_ip ):
+    try:
+        jsonString = urllib.urlopen("http://www.trackip.net/ip?json").read()
+        d = json.loads(jsonString)
+        current_ip = d['ip']
+        if stored_ip != current_ip:
+            email_new_ip( jsonString )
+            store_details( jsonString )
+    except:
+        print "Could not fetch IP: ", sys.exc_info()[0]
+
+def email_new_ip( details ):
+    print details
+    #!!!!!!!!!!!!!!  Change these details  !!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!! can use list for recip !!!!!!!!!!!!!!
+    send_email('user', 'pwd', 'recipient', 'Ip Change', details)
+
+def store_details ( details ):
+    f = open('.ip_json_details', 'w+')
+    f.write(details)
+    f.close()
+
+try:
+    f = open('.ip_json_details', 'r')
+    details = f.readline()
+    f.close()
+    d = json.loads(details)
+    compare_stored_ip( d['ip'] )
+except IOError as e:
+    #print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    f = open('.ip_json_details', 'w+')
+    f.write(default_details)
+    f.close()
+    d = json.loads(default_details)
+    compare_stored_ip( d['ip'] )
+# except ValueError:
+#     print "Could not convert data to an integer."
+except:
+    print "Unexpected error:", sys.exc_info()[0]
+    raise
