75b56d889a054be46e66a370c016ae7995418e84
ruanpienaar <ruan800@gmail.com>
2019-01-01 20:45:51 +0000

    Flaskr WIP

--- flask_apps/helloworld/flaskr/__init__.py
+++ flask_apps/helloworld/flaskr/__init__.py
@@ -23,9 +23,20 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
+    # Routes
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    # Db Initialize
+    from . import db
+    db.init_app(app)
+
+    # Blueprints:
+    from . import auth
+    app.register_blueprint(auth.bp)
+    from . import blog
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
 
     return app
